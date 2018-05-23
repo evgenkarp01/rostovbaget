a0528593fe5375c2a021084ab650bb295c1337ba
evgenkarp01 <ewal@list.ru>
2018-05-23 14:54:19 +0300

    final

--- js/custom.js
+++ js/custom.js
@@ -165,7 +165,24 @@ $(document).ready(function() {
     
     
     
+    $('.zoom-gallery').magnificPopup({
+    delegate: 'a',
+    type: 'image',
+    closeOnContentClick: false,
+    closeBtnInside: false,
+    mainClass: 'mfp-with-zoom mfp-img-mobile',
+    gallery: {
+      enabled: true
+    },
+    zoom: {
+      enabled: true,
+      duration: 300, // don't foget to change the duration also in CSS
+      opener: function(element) {
+        return element.find('img');
+      }
+    }
     
+  });
     
     
     
